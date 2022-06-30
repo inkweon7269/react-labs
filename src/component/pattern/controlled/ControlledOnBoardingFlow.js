52dc417d5b15b0263fac2a92f4c1d6580c119730
Inkwon Kim <inkweon7269@gmail.com>
2022-07-01 01:00:11 +0900

    출처 표기

--- src/component/pattern/controlled/ControlledOnBoardingFlow.js
+++ src/component/pattern/controlled/ControlledOnBoardingFlow.js
@@ -6,9 +6,13 @@ const ControlledOnBoardingFlow = ({ children, onFinish, currentIndex, onNext }) 
         onNext(stepData);
     }
 
+    // React.Children.toArray() : https://fe-developers.kakaoent.com/2021/211022-react-children-tip/
     const currentChild = React.Children.toArray(children)[currentIndex];
 
+    // isValidElement : https://webisfree.com/2020-08-26/[react]-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EC%9A%94%EC%86%8C-%EA%B2%80%EC%A6%9D%ED%95%98%EA%B8%B0-isvalidelement
     if (React.isValidElement(currentChild)) {
+
+        // React.cloneElement : https://developer-talk.tistory.com/233
         return React.cloneElement(currentChild, { goToNext });
     }
 
